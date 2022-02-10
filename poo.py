41b6ec4a1d35b1c7b95cf03f42b77650fb841f21
1gn1z_el_angel <kebor327@gmail.com>
2022-02-09 21:09:45 -0600

    3. atributos que tendrá la clase 'Camisetas'

--- poo.py
+++ poo.py
@@ -1,3 +1,21 @@
+"""
+Ejemplo de Clases:
+
+Clase Camisetas:
+
+Camiseta1: Marca: Nike | Talla: M | Precio 20.00
+Camiseta2: Marca: Gucci| Talla: L | Precio 799.99
+Camiseta3: Marca: Adidas|Talla: S | Precio 25.00
+
+"""
+
+
 # Creación de una clase con Python. Simplemente "class" + nombre de la clase + :
 class Camiseta:
-    pass
+# Atributos
+    talla = "XL"
+    color = "rojo"
+    precio = 19.99
+    marca = "KevorIndustries"
+
+
