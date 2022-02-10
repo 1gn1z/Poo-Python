aca1ff888e882c75969ef8e17ab7d8df2da44f77
1gn1z_el_angel <kebor327@gmail.com>
2022-02-09 21:27:30 -0600

    4. creación de nuestro primer objeto y acceso a sus atributos

--- poo.py
+++ poo.py
@@ -19,3 +19,58 @@ class Camiseta:
     marca = "KevorIndustries"
 
 
+# Creación de nuestra primera instancia de la clase "Camiseta"
+# Instanciamos en una variable un objeto de la clase "Camiseta" simplemente llamando la clase:
+
+
+camisetaSpiderman = Camiseta()  # Llamar a la clase (por su nombre tal cual) con Mayúscula! 
+
+
+# Acceso a los atributos (talla, color, etc.) de la clase ("Camiseta")
+# Accedemos con la nomenclatura del PUNTO!:
+
+camisetaSpiderman.talla
+camisetaSpiderman.color
+camisetaSpiderman.precio
+camisetaSpiderman.marca
+
+# NOTA: La nomenclatura del punto también sirve para llamar a los Métodos
+# Los atributos se llaman desde UN OBJETO O INSTANCIA de la clase!!!
+
+
+# Imprimimos los atributos de nuestra primera instancia u objeto:
+# (con un poco de formato)
+
+print(camisetaSpiderman.talla)
+
+print("Talla de la camiseta: " + camisetaSpiderman.talla)
+print("Color de la camiseta: " + camisetaSpiderman.color)
+print("Precio de la camiseta: " + str(camisetaSpiderman.precio))
+print("Marca de la camiseta: " + camisetaSpiderman.marca)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
